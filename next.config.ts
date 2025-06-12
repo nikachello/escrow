d7e1b1bd277902588348ca264cb538f9c3ce790e
nikachello <nikain35@gmail.com>
2025-06-12 15:49:10 +0400

    added shadcn/tailwind

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  plugins: {    "@tailwindcss/postcss": {},  },
 };
 
 export default nextConfig;
