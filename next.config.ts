56c6c8ba6182fe3d8570ad4ea3688402aa965d41
nikachello <nikain35@gmail.com>
2025-06-23 09:44:21 +0400

    test commit

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  plugins: {    "@tailwindcss/postcss": {},  },
+plugins: {    "@tailwindcss/postcss": {},  },     
 };
 
 export default nextConfig;
